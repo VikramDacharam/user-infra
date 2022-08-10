f1aa4b3a5ec6e494cbe869f2d86ebf10c886ae65
Vikram Dacharam <vikramdacharam95@gmail.com>
2022-08-10 21:35:09 +0530

    aa

---
+++ env-dev/backend.tfvars
@@ -0,0 +1,4 @@
+bucket = "terraform-b64"
+key    = "vpc/dev/user/terraform.tfstate"
+region = "us-east-1"
+
---
+++ main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+  source = "git::https://github.com/VikramDacharam/tf-module-vpc.git"
+  VPC_CIDR_BLOCK = var.VPC_CIDR_BLOCK
+  COMPONENT      = var.COMPONENT
+  ENV            = var.ENV
+  SUBNET_CIDR    = var.SUBNET_CIDR
+  AZ             = var.AZ
+}
